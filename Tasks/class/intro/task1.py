ea2027aa51fe5701b3302cfdcf96f48d23f5172d
MashinIvan <mivan19@yandex.ru>
2020-09-08 11:44:29 +0300

    tasks upd

---
+++ Tasks/class/intro/task1.py
@@ -0,0 +1,9 @@
+def foo():
+    test_array = ['eins', 24]  # comment 1
+    for item in test_array:
+        if item in [?, 2, 3]:
+            print?'a'?
+# Answer
+# 24
+# Type
+# input: 1; 1; 1
---
+++ Tasks/class/intro/task2.py
@@ -0,0 +1,12 @@
+def bar(test):
+    test += 1
+    return test
+
+age = 21
+print(bar(age))
+
+> ?
+# Answer
+# 22
+# Type
+# input: 2
